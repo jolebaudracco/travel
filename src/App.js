28b6c98e442f884255a4a2bf273344d9c8b0de0d
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-12-04 09:00:15 -0300

    Refactor app structure and remove unused files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  )
+}
+
+export default App
+
+const Logo = () => {
+  return <h1>🏝️ Far Away 🧳</h1>
+}
+
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  )
+}
+const PackingList = () => {
+  return (
+    <div className="list">
+      <ul className="list">
+        {initialItems.map((item) =>(
+          <Item item={item}/>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+const Item = ({item}) => {
+  return <li>{item.description}</li>
+}
+
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  )
+}
