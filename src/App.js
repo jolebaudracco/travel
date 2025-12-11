419da9ef2665b085d8a58bdb94a49ba207946eb3
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-12-11 15:49:41 -0300

    Add state management to Form component for item input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -24,15 +24,43 @@ const Logo = () => {
 }
 
 const Form = () => {
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    if(!description) return;
+    
+    const newItem = {
+      description: description,
+      quantity: quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select 
+        value={quantity} 
+        onChange={(e) => {setQuantity(Number(e.target.value));}}>
         {Array.from({length: 20}, (_, i) => i + 1).map(num => (
           <option value={num} key={num}>{num}</option>
         ))}
       </select>
-      <input type='text' placeholder='Item...' />
+      <input 
+        type='text' 
+        placeholder='Item...' 
+        value={description} 
+        onChange={(e) => {setDescription(e.target.value);}}
+      />
       <button>Add</button>
     </form>
   )
