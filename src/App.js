93b20c37675d7f13566b87120d7c7589c8512c73
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-12-17 09:59:52 -0300

    Implement item deletion functionality and update PackingList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+// ];
 
 const App = () => {
   const [items, setItems] = useState([])
@@ -13,11 +13,15 @@ const App = () => {
     setItems(items => [...items, item]);
   }
 
+  function handleDeleteItem(id){
+    setItems(items => items.filter(item => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItem}/>
-      <PackingList items={items}/>
+      <PackingList items={items} onDeleteItem={handleDeleteItem}/>
       <Stats />
     </div>
   )
@@ -73,25 +77,25 @@ const Form = ({onAddItems}) => {
     </form>
   )
 }
-const PackingList = ({items}) => {
+const PackingList = ({items, onDeleteItem}) => {
   return (
     <div className="list">
       <ul className="list">
         {items.map((item) =>(
-          <Item item={item} key={item.id}/>
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id}/>
         ))}
       </ul>
     </div>
   );
 }
 
-const Item = ({item}) => {
+const Item = ({item, onDeleteItem}) => {
   return (
     <li>
       <span style={item.packed ? {textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button  onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
