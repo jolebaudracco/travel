42bb1014106e9abdb03ae8c480aac90b6a76cd6a
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-12-19 09:32:00 -0300

    send all the components to their own file

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import Logo from './Logo';
+import Form from './Form';
+import PackingList from './PackingList';
+import Stats from './Stats';
+
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+// ];
+
+const App = () => {
+  const [items, setItems] = useState([])
+  
+
+  function handleAddItem(item){
+    setItems(items => [...items, item]);
+  }
+
+  function handleDeleteItem(id){
+    setItems(items => items.filter(item => item.id !== id));
+  }
+
+  function handleToggleItem(id){
+    setItems(items => 
+      items.map(item => 
+        item.id === id ? {...item, packed: !item.packed} : item
+      )
+    );
+  }
+
+  function handleClearList(){
+    const confirmed = window.confirm("Are you sure you want to clear the list?");
+    if(!confirmed) return;
+    setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItem}/>
+      <PackingList 
+        items={items} 
+        onDeleteItem={handleDeleteItem} 
+        onToggleItems={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items}/>
+    </div>
+  )
+}
+export default App
